52539b151466640b5c75b69df842435f33abdbfb
makidelille <makidelille@hotmail.fr>
2020-05-28 22:34:46 +0200

    💄(style) + logs

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const ImposeService = require("./impose");
 
 const PORT = process.env.PORT || 3000;
@@ -7,9 +8,9 @@ const app =  express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded());
+app.use(morgan("short"))
 
 app.use("/", express.static("./static"));
-
 
 function renderFactory(page, optParser){
     return (req, res) => res.render(page, Object.assign({},  optParser ? optParser(req): {}, {impose: ImposeService.getInstance() }))
@@ -22,8 +23,14 @@ app.get("/image/:img", (req, res, next) => {
 
 app.get("/", renderFactory("posts.ejs", req => ({page: 1})));
 app.get("/post/:page", renderFactory("posts.ejs", req => ({page: +req.params.page})));
-app.get("/home", renderFactory("posts.ejs"));
+app.get("/random", renderFactory("random.ejs"));
 app.get("/about", renderFactory("about.ejs"));
+app.get("*", renderFactory("error.ejs"));
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.render("error.ejs", {impose: ImposeService.getInstance() });
+});
+
 
 
 app.listen(PORT, () => {
